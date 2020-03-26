8015b7a308322f3b0cc8342c8072a648962c1c01
David Chan <chdavid@ca.ibm.com>
2020-03-26 16:29:41 -0400

    Make jaxrsmonitor report high and low values in the mbean

--- dev/com.ibm.ws.jaxrs.2.x.monitor/src/com/ibm/websphere/jaxrs/monitor/RestStatsMXBean.java
+++ dev/com.ibm.ws.jaxrs.2.x.monitor/src/com/ibm/websphere/jaxrs/monitor/RestStatsMXBean.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,63 +12,88 @@ package com.ibm.websphere.jaxrs.monitor;
 
 import com.ibm.websphere.monitor.jmx.Counter;
 import com.ibm.websphere.monitor.jmx.StatisticsMeter;
+import com.ibm.websphere.monitor.meters.Gauge;
 
 /**
- * Management12 interface for MBeans with names of the form "WebSphere:type=RestStats,name=*"
- * where * is the name of a RESTful resource method within an application under the Liberty profile of the form <appName>.<resourceMethodName>. For example, myApp.DemoResource. One such MBean for each resource method in the system is available
- * from the Liberty profile platform MBean server when the monitor-1.0 feature is enabled. This interface can be used to request a proxy object via the {@link javax.management.JMX#newMMBeanProxy} method.
+ * Management12 interface for MBeans with names of the form
+ * "WebSphere:type=RestStats,name=*" where * is the name of a RESTful resource
+ * method within an application under the Liberty profile of the form
+ * <appName>.<resourceMethodName>. For example, myApp.DemoResource. One such
+ * MBean for each resource method in the system is available from the Liberty
+ * profile platform MBean server when the monitor-1.0 feature is enabled. This
+ * interface can be used to request a proxy object via the
+ * {@link javax.management.JMX#newMMBeanProxy} method.
  * 
  * @ibm-api
  */
 public interface RestStatsMXBean {
 
-    /**
-     * Retrieves the value of the read-only attribute Description, which is a description of the MBean itself.
-     * 
-     * @return description
-     */
-    public String getDescription();
+	/**
+	 * Retrieves the value of the read-only attribute Description, which is a
+	 * description of the MBean itself.
+	 * 
+	 * @return description
+	 */
+	public String getDescription();
 
-    /**
-     * Retrieves the value of the read-only attribute MethodName, the name of the resource method as specified in the deployment descriptor.
-     * 
-     * @return method name
-     */
-    public String getMethodName();
+	/**
+	 * Retrieves the value of the read-only attribute MethodName, the name of the
+	 * resource method as specified in the deployment descriptor.
+	 * 
+	 * @return method name
+	 */
+	public String getMethodName();
 
-    /**
-     * Retrieves the value of the read-only attribute RequestCount, the number of requests the server has received for this resource method.
-     * 
-     * @return request count
-     */
-    public long getRequestCount();
+	/**
+	 * Retrieves the value of the read-only attribute RequestCount, the number of
+	 * requests the server has received for this resource method.
+	 * 
+	 * @return request count
+	 */
+	public long getRequestCount();
 
-    /**
-     * Retrieves the value of the read-only attribute RequestCountDetails, which provides other details on the request count.
-     * 
-     * @return request count details
-     */
-    public Counter getRequestCountDetails();
+	/**
+	 * Retrieves the value of the read-only attribute RequestCountDetails, which
+	 * provides other details on the request count.
+	 * 
+	 * @return request count details
+	 */
+	public Counter getRequestCountDetails();
 
-    /**
-     * Retrieves the value of the read-only attribute ResponseTime, which is the average (mean) time spent responding to each request for the resource method.
-     * 
-     * @return response time
-     */
-    public double getResponseTime();
+	/**
+	 * Retrieves the value of the read-only attribute ResponseTime, which is the
+	 * average (mean) time spent responding to each request for the resource method.
+	 * 
+	 * @return response time
+	 */
+	public double getResponseTime();
 
-    /**
-     * Retrieves the value of the read-only attribute ResponseCountDetails, which provides statistical details on the response time.
-     * 
-     * @return response time details
-     */
-    public StatisticsMeter getResponseTimeDetails();
+	/**
+	 * Retrieves the value of the read-only attribute ResponseCountDetails, which
+	 * provides statistical details on the response time.
+	 * 
+	 * @return response time details
+	 */
+	public StatisticsMeter getResponseTimeDetails();
 
-    /**
-     * Retrieves the value of the read-only attribute AppName, the name of the application of which the resource method belongs.
-     * 
-     * @return app name
-     */
-    public String getAppName();
+	public Gauge getMinuteLatestMinimumDuration();
+
+	public Gauge getMinuteLatestMaximumDuration();
+
+	public Gauge getMinuteLatest();
+
+	public Gauge getMinutePreviousMinimumDuration();
+
+	public Gauge getMinutePreviousMaximumDuration();
+
+	public Gauge getMinutePrevious();
+
+	/**
+	 * Retrieves the value of the read-only attribute AppName, the name of the
+	 * application of which the resource method belongs.
+	 * 
+	 * @return app name
+	 */
+	public String getAppName();
 
 }
