057119889b3d3d83d4559ed2a35435186d564239
David Chan <chdavid@ca.ibm.com>
2024-07-30 21:14:12 -0400

    WIPp

--- dev/io.openliberty.http.monitor/src/io/openliberty/http/monitor/ServletFilter.java
+++ dev/io.openliberty.http.monitor/src/io/openliberty/http/monitor/ServletFilter.java
@@ -12,6 +12,7 @@ package io.openliberty.http.monitor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Enumeration;
 
 import javax.servlet.UnavailableException;
 
@@ -24,6 +25,8 @@ import com.ibm.wsspi.webcontainer.webapp.IWebAppDispatcherContext;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -36,16 +39,21 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ServletFilter implements Filter {
 	
+	
 	private static final TraceComponent tc = Tr.register(ServletFilter.class);
 	
 	private static final String REST_HTTP_ROUTE_ATTR = "REST.HTTP.ROUTE";
+	
+	
+    @Override
+    public void init(FilterConfig config) {
+    	
+    }
 	
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
 			throws IOException, ServletException {
 
-		System.out.println("Do the filter");
-		
 		String appName = servletRequest.getServletContext().getAttribute("com.ibm.websphere.servlet.enterprise.application.name").toString();
 
 		Exception servletException = null;
@@ -108,136 +116,52 @@ public class ServletFilter implements Filter {
 			 * 3. Servlets
 			 */
 			if (httpRoute == null) {
+				
+				String nhp = null;
 				if (servletRequest instanceof SRTServletRequest) {
 					SRTServletRequest srtServletRequest = (SRTServletRequest) servletRequest;
-
-					IWebAppDispatcherContext webAppdispatcherContext = srtServletRequest.getWebAppDispatcherContext();
-
-					// Cast to WebAppDispatcherContext40 to obtain HttpServletMapping
-					if (webAppdispatcherContext instanceof WebAppDispatcherContext40) {
-						WebAppDispatcherContext40 webAppDispatcherContext40 = (WebAppDispatcherContext40) webAppdispatcherContext;
-						
-						//Can be null for direct match with servlet URL pattern with no wildcard
-						//OR 
-						String pathInfo = webAppDispatcherContext40.getPathInfo();
-
-						HttpServletMapping httpServletMapping = webAppDispatcherContext40.getServletMapping();
-						if (httpServletMapping != null && pathInfo != null) {
-
-							/*
-							 * Mapping exists:
-							 * 
-							 * 1. Could be an archive with JAX-RS/Restful resources in which case a
-							 * non-existent URL can still produce a HttpServletMapping with matchValue=""
-							 * and Pattern="/" but the _pathInfo of wadc40 would be a non empty path
-							 * 
-							 * 2. Wild card servlet match
-							 */
-							String pattern = httpServletMapping.getPattern();
-							String matchValue = httpServletMapping.getMatchValue();
-
-							/*
-							 * Pattern is "/", match value is empty "" and path info is NOT "/"
-							 * 
-							 * This occurs if we hit a RESTful/JAX-RS configured application
-							 * and a non-existent path was encountered.
-							 * 
-							 * (i.e. if /RestfulApp/nonExistent
-							 * 
-							 * This produces a pattern of "/", no match and pathInfo of "/"
-							 */
-							if (pattern.equals("/") && matchValue.isEmpty() && !pathInfo.equals("/")) {
-								httpRoute = contextPath + "/*";
-							}
-							/*
-							 * Pattern is "/*" but matchValue is ""
-							 * Instead of just using "/*", we set it as context-path only
-							 * 
-							 * e.g. Servlet deployed in applcation "MyApp" with pattern /*
-							 * and the request is sent to "/MyApp/" or "/MyApp"
-							 * We will just produce the httpRoute as "/MyApp"
-							 * 
-							 * This also applies to the MP Metrics "/metrics" endpoint
-							 */
-							else if (pattern.equals("/*") && matchValue.isEmpty()) {
-								httpRoute = contextPath;
-							}
-							/*
-							 * Special case for MP Health's "/health" pattern is empty and match value is empty, but the pathInfo is "/"
-							 * This is different with a direct match with a servlet url pattern where pathInfo is empty
-							 *  pattern contains the url pattern and matchValue is not empty (should match the url pattern)
-							 * /health
-							 */
-							else if (pathInfo.equals("/") && pattern.isEmpty() && matchValue.isEmpty()) {
-								httpRoute = contextPath;
-							}
-							/*
-							 *  A Match!  -- match for Servlet with a WILD CARD!
-							 */
-							else if (pattern != null && pattern.endsWith("/*") && !matchValue.isEmpty()) {
-								httpRoute = contextPath + pattern;
-							} else {
-								if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()){
-									Tr.debug(tc, String.format("Can not resolve HTTP route. Pattern:[%s] matchValue:[%s] pathInfo:[%s] ", pattern,matchValue,pathInfo));
-								}
-							}
-						} else if (pathInfo != null){
-
-							/*
-							 * Non Restful bound application
-							 * 
-							 * 1. Non-existent Path
-							 * 2. Welcome page?
-							 */
-
-							// Non existent path -> /context-path/*
-							if (!pathInfo.equals("/")) {
-								httpRoute = contextPath + "/*";
-								httpStatsAttributesHolder.setHttpRoute(httpRoute);
-							}
-							// Default page of some sort -> /context-path/ or file not found
-							else if (pathInfo.equals("/")) {
-								httpRoute = contextPath + "/";
-								httpStatsAttributesHolder.setHttpRoute(httpRoute);
-							} else {
-								if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()){
-									Tr.debug(tc, String.format("Can not resolve HTTP route.  pathInfo:[%s] ",pathInfo));
-								}
-							}
-						} else if (httpServletMapping != null && pathInfo == null){ // A SERVLET!!
-							String pattern = httpServletMapping.getPattern();
-							String matchValue = httpServletMapping.getMatchValue();
-							if (pattern != null && !pattern.equals("/") && !matchValue.isEmpty()) {
-								
-								pattern = (pattern.startsWith("/")) ? pattern : "/" + pattern;
-								httpRoute = contextPath + pattern;
-							} else {
-								if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()){
-									Tr.debug(tc, String.format("Can not resolve HTTP route.  matchValue:[%s] pathInfo:[%s] Pattern:[%s]", matchValue,pathInfo, pattern));
-								}
-								httpRoute = null;
-							}
-						} else if ((pathInfo == null) && 
-								(webAppDispatcherContext40.getServletPathForMapping() != null) 
-								&& (!webAppDispatcherContext40.getServletPathForMapping().isEmpty())){
-							
-							/*
-							 * PathInfo is null, httpServlet Mapping is null.
-							 * If we can get a servletPathForMapping value.
-							 * We'll use that.
-							 * 
-							 * Example: JSP that wasn't configured in web.xml
-							 */
-							String path = webAppDispatcherContext40.getServletPathForMapping();
-							path = (path.startsWith("/")) ? path : "/" + path ;
-							httpRoute = contextPath + webAppDispatcherContext40.getServletPathForMapping();
-							
-						}
-
-					} // cast to WebAppDispatcherContext40
-				} // cast to SRTServletRequest
+					
+					
+					String pathInfo = srtServletRequest.getPathInfo();
+					String servletPath = srtServletRequest.getServletPath();
+					String fullRequestURI = srtServletRequest.getRequestURI();
+					
+					System.out.println("\n################################## --------------- ");
+					System.out.println("srtservReqeust PathInfo: " + pathInfo);
+					System.out.println("srtservReqeust ServletPath: " + servletPath);
+					System.out.println("srtservReqeust contxtpath: " + contextPath);
+					System.out.println("srtservReqeust getRequestURI: " + fullRequestURI);
+					System.out.println("----------------------------------------------- ");
+					
+					
+					/*
+					 * path Info is null (no extrra path info) and servlet path is not null
+					 */
+					if (pathInfo == null && servletPath != null && !servletPath.isEmpty()) {
+						//TODO: double check that it starts with /?
+						nhp = contextPath + servletPath;
+					} 
+					/*
+					 * Special- like /metrics or /health
+					 * PathInfo is "/" 
+					 */
+					else if (pathInfo == "/" && servletPath != null && servletPath.isEmpty()) {
+						nhp = fullRequestURI;
+					}
+					
+					//no idea what it is
+					else if (servletPath != null  && servletPath.isEmpty() && pathInfo != null && pathInfo != "/" && !pathInfo.isEmpty()) {
+						nhp = contextPath  + "/*";
+					}
+					
+					System.out.println("the NEW http route is resolved to ---> " + nhp);
+					
+					
+					httpRoute = oldResolveHttpRoute(httpRoute, srtServletRequest, contextPath, httpStatsAttributesHolder);
+				}
+				
 			} // if httpRoute null
-
+			
 			httpStatsAttributesHolder.setHttpRoute(httpRoute);
 
 			/*
@@ -265,8 +189,9 @@ public class ServletFilter implements Filter {
 		
 		// Retrieve the HTTP request attributes
 		if (HttpServletRequest.class.isInstance(servletRequest)) {
+			
 			HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-
+			
 			httpStat.setRequestMethod(httpServletRequest.getMethod());
 
 			httpStat.setScheme(httpServletRequest.getScheme());
@@ -361,5 +286,143 @@ public class ServletFilter implements Filter {
 
 
 	}
+	
+	private String oldResolveHttpRoute(String httpRoute, SRTServletRequest srtServletRequest, String contextPath, HttpStatAttributes httpStatsAttributesHolder) {
+			
+			IWebAppDispatcherContext webAppdispatcherContext = srtServletRequest.getWebAppDispatcherContext();
+
+			// Cast to WebAppDispatcherContext40 to obtain HttpServletMapping
+			if (webAppdispatcherContext instanceof WebAppDispatcherContext40) {
+				WebAppDispatcherContext40 webAppDispatcherContext40 = (WebAppDispatcherContext40) webAppdispatcherContext;
+				
+				//Can be null for direct match with servlet URL pattern with no wildcard
+				//OR 
+				String pathInfo = webAppDispatcherContext40.getPathInfo();
+
+				HttpServletMapping httpServletMapping = webAppDispatcherContext40.getServletMapping();
+				if (httpServletMapping != null && pathInfo != null) {
+
+					/*
+					 * Mapping exists:
+					 * 
+					 * 1. Could be an archive with JAX-RS/Restful resources in which case a
+					 * non-existent URL can still produce a HttpServletMapping with matchValue=""
+					 * and Pattern="/" but the _pathInfo of wadc40 would be a non empty path
+					 * 
+					 * 2. Wild card servlet match
+					 */
+					String pattern = httpServletMapping.getPattern();
+					String matchValue = httpServletMapping.getMatchValue();
+					
+					System.out.println("pattern " + pattern);
+					System.out.println("matchValue " + matchValue);
+					/*
+					 * Pattern is "/", match value is empty "" and path info is NOT "/"
+					 * 
+					 * This occurs if we hit a RESTful/JAX-RS configured application
+					 * and a non-existent path was encountered.
+					 * 
+					 * (i.e. if /RestfulApp/nonExistent
+					 * 
+					 * This produces a pattern of "/", no match and pathInfo of "/"
+					 */
+					if (pattern.equals("/") && matchValue.isEmpty() && !pathInfo.equals("/")) {
+						httpRoute = contextPath + "/*";
+					}
+					/*
+					 * Pattern is "/*" but matchValue is ""
+					 * Instead of just using "/*", we set it as context-path only
+					 * 
+					 * e.g. Servlet deployed in applcation "MyApp" with pattern /*
+					 * and the request is sent to "/MyApp/" or "/MyApp"
+					 * We will just produce the httpRoute as "/MyApp"
+					 * 
+					 * This also applies to the MP Metrics "/metrics" endpoint
+					 */
+					else if (pattern.equals("/*") && matchValue.isEmpty()) {
+						httpRoute = contextPath;
+					}
+					/*
+					 * Special case for MP Health's "/health" pattern is empty and match value is empty, but the pathInfo is "/"
+					 * This is different with a direct match with a servlet url pattern where pathInfo is empty
+					 *  pattern contains the url pattern and matchValue is not empty (should match the url pattern)
+					 * /health
+					 */
+					else if (pathInfo.equals("/") && pattern.isEmpty() && matchValue.isEmpty()) {
+						httpRoute = contextPath;
+					}
+					/*
+					 *  A Match!  -- match for Servlet with a WILD CARD!
+					 */
+					else if (pattern != null && pattern.endsWith("/*") && !matchValue.isEmpty()) {
+						httpRoute = contextPath + pattern;
+					} else {
+						if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()){
+							Tr.debug(tc, String.format("Can not resolve HTTP route. Pattern:[%s] matchValue:[%s] pathInfo:[%s] ", pattern,matchValue,pathInfo));
+						}
+					}
+				} else if (pathInfo != null){
+
+					/*
+					 * Non Restful bound application
+					 * 
+					 * 1. Non-existent Path
+					 * 2. Welcome page?
+					 */
+
+					// Non existent path -> /context-path/*
+					if (!pathInfo.equals("/")) {
+						httpRoute = contextPath + "/*";
+						httpStatsAttributesHolder.setHttpRoute(httpRoute);
+					}
+					// Default page of some sort -> /context-path/ or file not found
+					else if (pathInfo.equals("/")) {
+						httpRoute = contextPath + "/";
+						httpStatsAttributesHolder.setHttpRoute(httpRoute);
+					} else {
+						if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()){
+							Tr.debug(tc, String.format("Can not resolve HTTP route.  pathInfo:[%s] ",pathInfo));
+						}
+					}
+				} else if (httpServletMapping != null && pathInfo == null){ // A SERVLET!!
+					String pattern = httpServletMapping.getPattern();
+					String matchValue = httpServletMapping.getMatchValue();
+					if (pattern != null && !pattern.equals("/") && !matchValue.isEmpty()) {
+						
+						pattern = (pattern.startsWith("/")) ? pattern : "/" + pattern;
+						httpRoute = contextPath + pattern;
+					} else {
+						if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()){
+							Tr.debug(tc, String.format("Can not resolve HTTP route.  matchValue:[%s] pathInfo:[%s] Pattern:[%s]", matchValue,pathInfo, pattern));
+						}
+						httpRoute = null;
+					}
+				} else if ((pathInfo == null) && 
+						(webAppDispatcherContext40.getServletPathForMapping() != null) 
+						&& (!webAppDispatcherContext40.getServletPathForMapping().isEmpty())){
+					
+					/*
+					 * PathInfo is null, httpServlet Mapping is null.
+					 * If we can get a servletPathForMapping value.
+					 * We'll use that.
+					 * 
+					 * Example: JSP that wasn't configured in web.xml
+					 */
+					String path = webAppDispatcherContext40.getServletPathForMapping();
+					path = (path.startsWith("/")) ? path : "/" + path ;
+					httpRoute = contextPath + webAppDispatcherContext40.getServletPathForMapping();
+					
+				}
+
+			} // cast to WebAppDispatcherContext40
+		System.out.println("(Existing algo) resolved the httpRoute to " + httpRoute);
+		return httpRoute;
+	}
+	
+	
+    /** {@inheritDoc} */
+    @Override
+    public void destroy() {
+    }
 
 }
